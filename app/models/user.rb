47e51008ae74e1e6982b2935345a5ad79fdad664
Ptotem Learning <info@ptotem.com>
2013-01-25 02:59:31 +0530

    add omniauthable controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,23 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :admin, :power, :mana, :score
 
-  has_many :games, :dependent=> :destroy
+  has_many :games, :dependent => :destroy
+
+  def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
+    data = access_token.extra.raw_info
+    if user = User.where(:email => data.email).first
+      user
+    else # Create a user with a stub password.
+      User.create!(:email => data.email, :password => Devise.friendly_token[0, 20])
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"]
+      end
+    end
+  end
 
 end
