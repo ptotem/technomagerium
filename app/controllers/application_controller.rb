a68f4914e8aae2c63f6a5120d2f3960c77e056b4
Ptotem Learning <info@ptotem.com>
2013-01-25 00:41:31 +0530

    Facebook Integration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,7 @@
 class ApplicationController < ActionController::Base
-  #protect_from_forgery
+  protect_from_forgery
+  before_filter :authenticate_user!
   before_filter :set_locale
-  before_filter :parse_facebook_cookies
-  #before_filter :authenticate_user!
-
-  def parse_facebook_cookies
-   #@facebook_cookies = Koala::Facebook::OAuth.new.get_user_info_from_cookie(cookies)
-   #@graph = Koala::Facebook::GraphAPI.new(@facebook_cookies["access_token"])
-  end
 
   def set_locale
     if is_a?(RailsAdmin::ApplicationController) or !user_signed_in?
