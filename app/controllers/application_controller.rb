ba181676b262fe85073d838e258aa0ad5c5a0d64
Ptotem Learning <info@ptotem.com>
2013-01-25 03:23:47 +0530

    Trying to make the sign in autoredirect to facebook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter do
-    authenticate_user! rescue redirect_to 'auth/facebook'
-  end
+  before_filter :authenticate_user!
   before_filter :set_locale
 
   def set_locale
