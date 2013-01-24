a87966772220fd0eac93d617184e8ed03b2bb7f0
Ptotem Learning <info@ptotem.com>
2013-01-25 01:28:12 +0530

    Working at FB Integration

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # The following is for facebook
-  provider :facebook, ["135450493285811"], ["2896033cae2b1ce1d396ac41c7ef8fe2"], {:scope => 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access'}
+  provider :facebook, [135450493285811], [2896033cae2b1ce1d396ac41c7ef8fe2], {:scope => 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access'}
 
   # If you want to also configure for additional login services, they would be configured here.
 end
