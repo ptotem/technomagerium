90ab5a6bbd4433d27744f23befd98c816c3265eb
Ptotem Learning <info@ptotem.com>
2013-01-25 12:53:22 +0530

    Updated Landing Splash

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,5 +4,19 @@ class WelcomeController < ApplicationController
 
   def library
     @puzzles=Puzzle.all
+
+    @graph = Koala::Facebook::API.new("AAAB7MQaJdbMBAAwYykwBEbOh4Lsq1PWFlt2TQkoHidhAvtLlxca1gJCbOp8pkpNx6PvhDr5EHuJjNCwmt90UQJnKvL9ZCGIZB9kWlJCwZDZD")
+
+    #@profile = @graph.get_object("me")
+    #@friends = @graph.get_connections("me", "friends")
+    #@graph.put_connections("me", "feed", :message => "Testing a facebook app. Please ignore.")
+
+    # three-part queries are easy too!
+    #@graph.get_connections("me", "mutualfriends/#{friend_id}")
+
+    # you can even use the new Timeline API
+    # see https://developers.facebook.com/docs/beta/opengraph/tutorial/
+    #@graph.put_connections("me", "namespace:action", :object => object_url)
+
   end
 end
