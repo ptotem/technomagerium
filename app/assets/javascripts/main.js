d2acbdfe8862ac93c9cd701ca6ab08c613c162c2
Ptotem Learning <info@ptotem.com>
2013-01-27 20:38:13 +0530

    Created First Tome

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -100,7 +100,7 @@ function createFields(alchemy_element_array) {
             'class':'field',
             'html':'<a href="#" title=' + alchemy_element_array[i] + '><img src="/assets/elements/' + alchemy_element_array[i] + '-off.png" class="alchemy_elements" id="' + i + '"/></a>'
         }).appendTo(container);
-        imageObj.src='/assets/elements/' + alchemy_element_array[i] + '-on.png';
+        imageObj.src = '/assets/elements/' + alchemy_element_array[i] + '-on.png';
     }
 }
 
@@ -264,73 +264,4 @@ function show_content(obj) {
         });
     }
 }
-
-var Page = (function () {
-
-    var config = {
-            $bookBlock:$('#bb-bookblock'),
-            $navNext:$('#bb-nav-next'),
-            $navPrev:$('#bb-nav-prev'),
-            $navJump:$('#bb-nav-jump'),
-            bb:$('#bb-bookblock').bookblock({
-                speed:800,
-                shadowSides:0.8,
-                shadowFlip:0.7
-            })
-        },
-        init = function () {
-
-            initEvents();
-
-        },
-        initEvents = function () {
-
-            var $slides = config.$bookBlock.children(),
-                totalSlides = $slides.length;
-
-            // add navigation events
-            config.$navNext.on('click', function () {
-
-                config.bb.next();
-                return false;
-
-            });
-
-            config.$navPrev.on('click', function () {
-
-                config.bb.prev();
-                return false;
-
-            });
-
-            config.$navJump.on('click', function () {
-
-                config.bb.jump(totalSlides);
-                return false;
-
-            });
-
-            // add swipe events
-            $slides.on({
-
-                'swipeleft':function (event) {
-
-                    config.bb.next();
-                    return false;
-
-                },
-                'swiperight':function (event) {
-
-                    config.bb.prev();
-                    return false;
-
-                }
-
-            });
-
-        };
-
-    return { init:init };
-
-})();
 
