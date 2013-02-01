36029f1e03ac95f584b48b74f1de77814afaecfc
Ptotem Learning <info@ptotem.com>
2013-02-01 13:31:32 +0530

    Still working on integrating story

--- app/models/story_page.rb
+++ app/models/story_page.rb
@@ -1,4 +1,4 @@
 class StoryPage < ActiveRecord::Base
-  attr_accessible :progress, :num, :user_id
+  attr_accessible :progress, :num, :user_id, :chapter_break
   belongs_to :user
 end
--- app/models/tome.rb
+++ app/models/tome.rb
@@ -1,5 +1,5 @@
 class Tome < ActiveRecord::Base
-  attr_accessible :completed, :name, :avatar, :puzzles_attributes, :base_back, :cover_page, :theme, :elements, :sequence, :openable, :chapter, :progress
+  attr_accessible :completed, :name, :avatar, :puzzles_attributes, :base_back, :cover_page, :theme, :elements, :sequence, :openable, :chapter, :beginning, :ending
   has_many :puzzles, :dependent => :destroy
   has_many :user_states
   has_attached_file :avatar
