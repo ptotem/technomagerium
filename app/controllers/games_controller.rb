baee9b11b81fe9b40a52c8a8c8057370e836883f
Ptotem Learning <info@ptotem.com>
2013-02-04 01:49:54 +0530

    Did the navigation through the puzzle

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -139,10 +139,20 @@ class GamesController < ApplicationController
       when "explanation"
         @tome_completion = @game.puzzle.tome.puzzles.count - current_user.games.where('solved=?', true).map { |g| g.puzzle }.select { |p| p.tome_id==@game.puzzle.tome.id }.count
         if @tome_completion==0
-          @redirection="/library/#{@game.puzzle.tome.chapter+1}"
+          @puzzle_count=Tome.where('chapter=?', @game.puzzle.tome.chapter).all.map { |t| t.puzzles.count }.sum
+          @game_count=Tome.where('chapter=?', @game.puzzle.tome.chapter).all.map { |t| t.puzzles }.flatten.map { |p| p.games.where(user_id: current_user.id) }.flatten.select { |g| g.solved }.count
+          if @puzzle_count==@game_count
+            @redirection="/library/#{@game.puzzle.tome.chapter+1}"
+          else
+            @redirection="/knowledge/#{Tome.where('chapter=?', @game.puzzle.tome.chapter).where(theme: "").first.id}/end"
+          end
+          unless current_user.user_state.tome==Tome.find_by_sequence(@game.puzzle.tome.sequence+1)
+            current_user.user_state.tome_id=Tome.find_by_sequence(@game.puzzle.tome.sequence+1).id
+            current_user.user_state.save
+          end
         else
           @incomplete_puzzles=(@game.puzzle.tome.puzzles - current_user.games.where('solved=?', true).map { |g| g.puzzle }.select { |p| p.tome_id==@game.puzzle.tome.id }.map { |p| p }).sort
-          if (@game.puzzle.sequence+1)>@incomplete_puzzles.map{|p| p.sequence}.max
+          if (@game.puzzle.sequence+1)>@incomplete_puzzles.map { |p| p.sequence }.max
             @redirection="/play/#{@incomplete_puzzles.first.id}"
           else
             @target=@game.puzzle.sequence
@@ -166,29 +176,30 @@ class GamesController < ApplicationController
     @bitmask=params[:bitmask]
     if @bitmask==@game.puzzle.combo
       @game.lore=true
-      @game.solved=true
-      if @game.save
-        @tome_completion = @game.puzzle.tome.puzzles.count - @user.games.where('solved=?', true).map { |g| g.puzzle }.select { |p| p.tome_id==@game.puzzle.tome.id }.count
-        if @tome_completion == 0
-          UserState.create!(user_id: @user.id, tome_id: @game.puzzle.tome.id)
-          @last_page=StoryPage.find_all_by_user_id(@user.id).last.num rescue 0
-          @game.puzzle.tome.ending.split("||").each_with_index do |e, index|
-            StoryPage.create!(user_id: @user.id, num: (@last_page+index+1), progress: e)
-          end
-          if Tome.find_by_sequence_and_chapter(@game.puzzle.tome.sequence+1, @game.puzzle.tome.chapter).blank?
-            StoryPage.create!(user_id: @user.id, num: (StoryPage.find_all_by_user_id(@user.id).last.num+1), progress: "<div class='chapter_end'><a href='/library/#{(@game.puzzle.tome.chapter+1)}'>Next Chapter</a></div>")
-          end
-          StoryPage.create!(user_id: @user.id, num: (StoryPage.find_all_by_user_id(@user.id).last.num+1 rescue 1), progress: (@game.puzzle.tome.sequence+1))
-          @last_page=StoryPage.find_all_by_user_id(@user.id).last.num
-          @next_tome=Tome.find_by_sequence_and_chapter(@game.puzzle.tome.sequence+1, @game.puzzle.tome.chapter)
-          @game.puzzle.tome.beginning.split("||").each_with_index do |e, index|
-            StoryPage.create!(user_id: @user.id, num: (@last_page+index+1), progress: e)
+      unless @game.solved
+        @game.solved=true
+        if @game.save
+          @tome_completion = @game.puzzle.tome.puzzles.count - @user.games.where('solved=?', true).map { |g| g.puzzle }.select { |p| p.tome_id==@game.puzzle.tome.id }.count
+          if @tome_completion == 0
+            @last_page=StoryPage.find_all_by_user_id(@user.id).last.num rescue 0
+            @game.puzzle.tome.ending.split("||").each_with_index do |e, index|
+              StoryPage.create!(user_id: @user.id, num: (@last_page+index+1), progress: e)
+            end
+            if Tome.find_by_sequence_and_chapter(@game.puzzle.tome.sequence+1, @game.puzzle.tome.chapter).blank?
+              StoryPage.create!(user_id: @user.id, num: (StoryPage.find_all_by_user_id(@user.id).last.num+1), progress: "<div class='chapter_end'><a href='/library/#{(@game.puzzle.tome.chapter+1)}'>Next Chapter</a></div>")
+            end
+            StoryPage.create!(user_id: @user.id, num: (StoryPage.find_all_by_user_id(@user.id).last.num+1 rescue 1), progress: (@game.puzzle.tome.sequence+1))
+            @last_page=StoryPage.find_all_by_user_id(@user.id).last.num
+            @next_tome=Tome.find_by_sequence_and_chapter(@game.puzzle.tome.sequence+1, @game.puzzle.tome.chapter)
+            @game.puzzle.tome.beginning.split("||").each_with_index do |e, index|
+              StoryPage.create!(user_id: @user.id, num: (@last_page+index+1), progress: e)
+            end
           end
         end
+        @user.mana+=@game.puzzle.mana_reward
+        @user.power+=@game.puzzle.power_reward
+        @user.save
       end
-      @user.mana+=@game.puzzle.mana_reward
-      @user.power+=@game.puzzle.power_reward
-      @user.save
 
       render :text => "y||#{@user.mana}||#{@user.power}"
     else
--- app/controllers/tomes_controller.rb
+++ app/controllers/tomes_controller.rb
@@ -11,5 +11,6 @@ class TomesController < ApplicationController
     @tome=Tome.find(params[:id])
     @next_tome=Tome.find_by_sequence(@tome.sequence+1)
     @story_pages=current_user.story_pages.all
+    gon.jump_to_end= (params[:end].blank? ? false : true)
   end
 end
